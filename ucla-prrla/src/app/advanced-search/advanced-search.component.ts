f5b59715197712fb560dd771e0b3b5773ec2ec01
saslv <yar.hasko@gmail.com>
2017-08-04 17:56:29 +0300

    added page size option

--- ucla-prrla/src/app/advanced-search/advanced-search.component.ts
+++ ucla-prrla/src/app/advanced-search/advanced-search.component.ts
@@ -5,176 +5,183 @@ import {element} from 'protractor';
 import {Subscription} from 'rxjs/Subscription';
 
 @Component({
-  selector: 'app-advanced-search',
-  templateUrl: './advanced-search.component.html',
+    selector: 'app-advanced-search',
+    templateUrl: './advanced-search.component.html',
 })
 
 export class AdvancedSearchComponent implements OnInit {
-  // books: Array<any>;
-  // error: string;
-  // displayValues = [];
-  // pageOfItems: number;
-  // changeShape;
-  //
-  // static changeShape (value?: number): void {}
-  //
-  // constructor(private http: Http, private booksService: BooksService) {
-  //
-  //   for (let i = 1; i <= 10; i++) {
-  //     this.displayValues.push(`${i}`)
-  //   }
-  //
-  // }
-  //
-  // clicked(event) {
-  //   event.target.classList.toggle('active'); // To toggle
-  // }
-  //
-  // ngOnInit() {
-  //   this.booksService.getAllBooks()
-  //     .subscribe(
-  //       data => this.books = data,
-  //       error => this.error = error.statusText,
-  //     )
-  // };
+    // books: Array<any>;
+    // error: string;
+    // displayValues = [];
+    // pageOfItems: number;
+    // changeShape;
+    //
+    // static changeShape (value?: number): void {}
+    //
+    // constructor(private http: Http, private booksService: BooksService) {
+    //
+    //   for (let i = 1; i <= 10; i++) {
+    //     this.displayValues.push(`${i}`)
+    //   }
+    //
+    // }
+    //
+    // clicked(event) {
+    //   event.target.classList.toggle('active'); // To toggle
+    // }
+    //
+    // ngOnInit() {
+    //   this.booksService.getAllBooks()
+    //     .subscribe(
+    //       data => this.books = data,
+    //       error => this.error = error.statusText,
+    //     )
+    // };
 
-  url_page: any;
-  route$: Subscription;
-  public items = [];
-  public itemFilters;
-  public pager;
+    url_page: any;
+    route$: Subscription;
+    public items = [];
+    public itemFilters;
+    public pager;
+    public pageSize;
 
-  public selectedFilters = {};
+    public selectedFilters = {};
 
-  public search_therms = '';
+    public search_therms = '';
 
-  constructor(
-      private testService: TestService,
-      private route: ActivatedRoute,
-      private router: Router
-  ) {
-  }
+    constructor(private testService: TestService,
+                private route: ActivatedRoute,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-    this.pager = this.testService.getPager(0);
+    ngOnInit() {
+        this.pager = this.testService.getPager(0);
+        this.pageSize = this.testService.pageSize;
 
-    this.route$ = this.route.queryParams.subscribe(
-        (params: Params) => {
-          let page = +params['page'];
-          if(!isNaN(page)){
-            this.url_page = page;
-          }
-          let params_json = params['filters'];
-          if(params_json){
-            this.selectedFilters = JSON.parse(params_json);
-          }else{
-            this.selectedFilters = {};
-          }
+        this.route$ = this.route.queryParams.subscribe(
+            (params: Params) => {
+                let page = +params['page'];
+                if (!isNaN(page)) {
+                    this.url_page = page;
+                }
+                let params_json = params['filters'];
+                if (params_json) {
+                    this.selectedFilters = JSON.parse(params_json);
+                } else {
+                    this.selectedFilters = {};
+                }
 
-          let therms = params['therms'];
-          if(therms){
-            this.search_therms = therms;
-          }
+                let therms = params['therms'];
+                if (therms) {
+                    this.search_therms = therms;
+                }
 
-          this.setPage(this.url_page);
+                this.setPage(this.url_page);
+            }
+        );
+
+        this.setPage(this.url_page);
+    }
+
+    ngOnDestroy() {
+        if (this.route$) {
+            this.route$.unsubscribe();
         }
-    );
-
-    this.setPage(this.url_page);
-  }
-
-  ngOnDestroy() {
-    if (this.route$) {
-      this.route$.unsubscribe();
-    }
-  }
-
-  setPage(page: number) {
-    document.getElementById('loading').style.display = 'block';
-    this.testService.getPaginatedBooks(this.search_therms, this.selectedFilters, page).subscribe(data => {
-      this.items = data.items;
-      this.pager = this.testService.getPager(data.totalItems, page);
-      this.itemFilters = data.itemFilters;
-      document.getElementById('loading').style.display = 'none';
-    });
-  }
-
-  toggleActiveClass(event) {
-    event.target.classList.toggle('active');
-  }
-
-  pagerClick(event, page) {
-    if(!event.target.parentElement.classList.contains('disabled')){
-      this.navigateWithParams(page, this.selectedFilters, this.search_therms);
-    }
-  }
-
-  navigateWithParams(page, filters, therms){
-    this.router.navigate(['/advanced-search'], { queryParams: {
-      page: page,
-      filters: JSON.stringify(filters),
-      therms: therms
-    } });
-  }
-
-  searchOnEnter(event) {
-    this.search_therms = event.target.value;
-    this.setPage(1);
-    this.navigateWithParams(1, this.selectedFilters, this.search_therms);
-  }
-
-  searchButtonClick(therms: string) {
-    this.search_therms = therms;
-    this.setPage(1);
-    this.navigateWithParams(1, this.selectedFilters, this.search_therms);
-  }
-
-  clickOnFilter(event) {
-    let filterName = event.target.name;
-    let filterValue = event.target.value;
-    let checked = event.target.checked;
-
-    if(typeof this.selectedFilters[filterName] === 'undefined'){
-      this.selectedFilters[filterName] = [];
     }
 
-    let index = this.selectedFilters[filterName].indexOf(filterValue);
-
-    if(checked){
-      if(index === -1){
-        this.selectedFilters[filterName].push(filterValue);
-      }
-    }else{
-      if(index > -1){
-        this.selectedFilters[filterName].splice(index, 1);
-      }
+    setPage(page: number) {
+        document.getElementById('loading').style.display = 'block';
+        this.testService.getPaginatedBooks(this.search_therms, this.selectedFilters, page).subscribe(data => {
+            this.items = data.items;
+            this.pager = this.testService.getPager(data.totalItems, page);
+            this.itemFilters = data.itemFilters;
+            document.getElementById('loading').style.display = 'none';
+        });
     }
 
-    if(this.selectedFilters[filterName].length == 0){
-      delete this.selectedFilters[filterName];
+    pageSizeChange() {
+        this.testService.pageSize = this.pageSize;
+        this.setPage(1);
     }
 
-    this.navigateWithParams(1, this.selectedFilters, this.search_therms);
-  }
-
-  getIsSelectedFilter(filterName, filterVal){
-    if (typeof this.selectedFilters[filterName] === 'undefined'){
-      return false;
+    toggleActiveClass(event) {
+        event.target.classList.toggle('active');
     }
 
-    if (this.selectedFilters[filterName].indexOf(filterVal) === -1){
-      return false;
+    pagerClick(event, page) {
+        if (!event.target.parentElement.classList.contains('disabled')) {
+            this.navigateWithParams(page, this.selectedFilters, this.search_therms);
+        }
     }
 
-    return true;
-  }
-
-  getIsSelectedFilterChecked(filterName, filterVal){
-    if(this.getIsSelectedFilter(filterName, filterVal)){
-      return 'checked';
-    }else{
-      return '';
+    navigateWithParams(page, filters, therms) {
+        this.router.navigate(['/advanced-search'], {
+            queryParams: {
+                page: page,
+                filters: JSON.stringify(filters),
+                therms: therms
+            }
+        });
     }
-  }
+
+    searchOnEnter(event) {
+        this.search_therms = event.target.value;
+        this.setPage(1);
+        this.navigateWithParams(1, this.selectedFilters, this.search_therms);
+    }
+
+    searchButtonClick(therms: string) {
+        this.search_therms = therms;
+        this.setPage(1);
+        this.navigateWithParams(1, this.selectedFilters, this.search_therms);
+    }
+
+    clickOnFilter(event) {
+        let filterName = event.target.name;
+        let filterValue = event.target.value;
+        let checked = event.target.checked;
+
+        if (typeof this.selectedFilters[filterName] === 'undefined') {
+            this.selectedFilters[filterName] = [];
+        }
+
+        let index = this.selectedFilters[filterName].indexOf(filterValue);
+
+        if (checked) {
+            if (index === -1) {
+                this.selectedFilters[filterName].push(filterValue);
+            }
+        } else {
+            if (index > -1) {
+                this.selectedFilters[filterName].splice(index, 1);
+            }
+        }
+
+        if (this.selectedFilters[filterName].length == 0) {
+            delete this.selectedFilters[filterName];
+        }
+
+        this.navigateWithParams(1, this.selectedFilters, this.search_therms);
+    }
+
+    getIsSelectedFilter(filterName, filterVal) {
+        if (typeof this.selectedFilters[filterName] === 'undefined') {
+            return false;
+        }
+
+        if (this.selectedFilters[filterName].indexOf(filterVal) === -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    getIsSelectedFilterChecked(filterName, filterVal) {
+        if (this.getIsSelectedFilter(filterName, filterVal)) {
+            return 'checked';
+        } else {
+            return '';
+        }
+    }
 
 }
