faa8e88d16d7c6010f15840b583e8caecea60c0b
Vasyl Kruten <vasylkruten@gmail.com>
2017-09-27 20:12:28 +0300

    upd

--- ucla-prrla/src/app/truncate.pipe.ts
+++ ucla-prrla/src/app/truncate.pipe.ts
@@ -1,12 +1,13 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {isDefined} from "@angular/compiler/src/util";
 
 @Pipe({
     name: 'truncate'
 })
 export class TruncatePipe implements PipeTransform{
-    transform(value: string, args: string[]) : string {
-        let limit = args.length > 0 ? parseInt(args[0], 10) : 10;
-        let trail = args.length > 1 ? args[1] : '...';
+    transform(value: string, args: string) : string {
+        let limit = parseInt(args);
+        let trail = '...';
 
         return value.length > limit ? value.substring(0, limit) + trail : value;
     }
