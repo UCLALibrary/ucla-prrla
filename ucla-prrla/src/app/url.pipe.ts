616325af7f061646e9eabfdd8eba7a37e4df97db
saslv <yar.hasko@gmail.com>
2017-10-06 17:56:08 +0300

    fixes

--- ucla-prrla/src/app/url.pipe.ts
+++ ucla-prrla/src/app/url.pipe.ts
@@ -3,11 +3,10 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name:'url'})
 export class UrlPipe implements PipeTransform {
     transform(value: string, arg?: any): any {
-        var exp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-        if (value) {
-            return value.replace(exp, "<a href='$1' target='_blank'>'$1'</a>");
+        if (value.indexOf('http') === 0) {
+            return '<a href="' + value + '" target="_blank">' + value + '</a>';
         } else {
-            return value.replace(exp, "<span>'$1'</span>");
+            return '<span>' + value + '</span>';
         }
     }
 }
