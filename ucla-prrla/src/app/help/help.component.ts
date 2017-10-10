05bfb3f1e219f4d0ec718b1ec16ad4dcadd4d9c1
Vasyl Kruten <vasylkruten@gmail.com>
2017-10-10 13:47:52 +0300

    added scroll

--- ucla-prrla/src/app/help/help.component.ts
+++ ucla-prrla/src/app/help/help.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-help',
@@ -6,13 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpComponent implements OnInit {
 
+  public navIsFixed: boolean = false;
+
   goTo(location: string): void {
     window.location.hash = location;
   }
 
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit() {
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    let number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 280) {
+      this.navIsFixed = true;
+    } else if (this.navIsFixed && number < 280) {
+      this.navIsFixed = false;
+    }
+  }
 }
