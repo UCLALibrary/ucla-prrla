c07324c8112ccab0d999b1ff8602fe02a099305c
Anthony Vuong <avuong@cachemeoutside.io>
2020-10-07 14:24:17 -0700

    Update prod to use https connection to solr backend (#99)

--- ucla-prrla/src/app/services/solr.service.ts
+++ ucla-prrla/src/app/services/solr.service.ts
@@ -45,7 +45,7 @@ export class SolrService {
      */
     constructor(private _jsonp: Jsonp) {
         if (environment.production) {
-            this.baseURL = 'http://solr.library.ucla.edu/solr/prl/';
+            this.baseURL = 'https://solr.library.ucla.edu/solr/prl/';
             console.log('prod');
         } else {
             this.baseURL = 'https://test-solr.library.ucla.edu/solr/prl/';
